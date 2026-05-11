59502644ae8d36af617d5f0450e12d25658974fd
Dhurga <dhurga.g2024cse2sece.ac.in>
2026-05-11 11:10:51 +0530

    added files

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 import sys
 import os
 
-# Add smart_transport project root to path
-BASE = os.path.join(os.path.dirname(__file__), '..', 'smart_transport')
-sys.path.insert(0, os.path.abspath(BASE))
-
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'smart_transport'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'smart_transport.settings'
 
-from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
+import django
+django.setup()
+
+from django.core.handlers.wsgi import WSGIHandler
+app = WSGIHandler()
